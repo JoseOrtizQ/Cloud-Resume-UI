7037b84275df210e223301cdff3ea2d5e1c01880
Jose Ortiz <joseortizgarciaq@gmail.com>
2025-06-04 21:48:48 -0400

    Hero section Header color changed.

--- react-portfolio/src/components/Hero.js
+++ react-portfolio/src/components/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
       </video>
       <div className="absolute inset-0 bg-black bg-opacity-60 z-0"></div>
       <div className="relative z-10 flex flex-col items-center space-y-8">
-        <h1 className="text-5xl md:text-7xl font-bold">Hi, I'm José</h1>
+        <h1 className="text-5xl md:text-7xl font-bold text-white">Hi, I'm José</h1>
         <section className="flex flex-wrap justify-center gap-11 text-4xl text-gray-300 drop-shadow-md">
           {[
             ['html5-plain-wordmark', 'HTML5'],
