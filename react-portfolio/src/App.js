6aa8ae9d4b010f8b1d30a46caeac110b214248a9
Jose Ortiz <joseortizgarciaq@gmail.com>
2025-07-24 22:30:44 -0400

    PageTransition.js deleted and App.js updated.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,7 +1,6 @@
-// Replace your current App.js with this:
+// App.js without PageTransition
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import PageTransition from './components/PageTransition';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
@@ -11,42 +10,28 @@ import About from './components/About';
 import Contact from './components/Contact';
 import ProjectsPageComponent from './components/ProjectsPage';
 
-// Wrapper component to access location
-const AppContent = () => {
-  const location = useLocation();
-
-  // Create a Home component that contains all your current sections
-  const Home = () => {
-    return (
-      <>
-        <Navbar />
-        <Hero />
-        <Projects />
-        <Education />
-        <Certifications /> 
-        <About />
-        <Contact />
-      </>
-    );
-  };
-
-  // Create a Projects page layout that includes Navbar
-  const ProjectsPage = () => {
-    return (
-      <>
-        <Navbar />
-        <ProjectsPageComponent />
-      </>
-    );
-  };
-
+// Create a Home component that contains all your current sections
+const Home = () => {
   return (
-    <PageTransition location={location.pathname}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-      </Routes>
-    </PageTransition>
+    <>
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Education />
+      <Certifications /> 
+      <About />
+      <Contact />
+    </>
+  );
+};
+
+// Create a Projects page layout that includes Navbar
+const ProjectsPage = () => {
+  return (
+    <>
+      <Navbar />
+      <ProjectsPageComponent />
+    </>
   );
 };
 
@@ -54,7 +39,10 @@ function App() {
   return (
     <Router>
       <div>
-        <AppContent />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
       </div>
     </Router>
   );
